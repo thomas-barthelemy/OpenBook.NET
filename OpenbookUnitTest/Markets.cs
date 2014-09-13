c6a11f1b2c7d4d73ce83607ba32ae34199db1e56
Thomas Barthelemy <thomasb@gobeta.com.cn>
2014-09-13 17:26:16 +0800

    Added base unit tests

---
+++ OpenbookUnitTest/Markets.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace OpenbookUnitTest
+{
+    /// <summary>
+    /// Markets related unit tests
+    /// </summary>
+    [TestClass]
+    public class Markets
+    {
+        [TestMethod]
+        public void GetTopStocks()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void GetTopIndicies()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void GetTopCurrencies()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void GetTopCommodities()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void GetCurrencyFeed()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void GetCommodityFeed()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ OpenbookUnitTest/People.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace OpenbookUnitTest
+{
+    /// <summary>
+    /// Peoples related unit tests
+    /// </summary>
+    [TestClass]
+    public class People
+    {
+        [TestMethod]
+        public void GetTopPeopleByCopiers()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void GetTopPeopleByGains()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void GetPeopleFeed()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void GetPeopleStats()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
