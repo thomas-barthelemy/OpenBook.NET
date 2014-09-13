37dc100d336c857944732ca69b0e5eec3fda5228
Thomas Barthelemy <thomasb@gobeta.com.cn>
2014-09-13 17:10:19 +0800

    Added UnitTest project for universal lib

---
+++ OpenbookUnitTest/Users.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace OpenbookUnitTest
+{
+    /// <summary>
+    /// Users related unit tests
+    /// </summary>
+    [TestClass]
+    public class Users
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
