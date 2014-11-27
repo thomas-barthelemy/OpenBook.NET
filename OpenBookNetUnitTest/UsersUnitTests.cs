73ba716f94751c9cc4c7c6b35094fa19b3938d2b
Thomas Barthelemy <thomasb@gobeta.com.cn>
2014-11-27 15:12:49 +0800

    UnitTests for TopPerformers API

--- OpenBookNetUnitTest/UsersUnitTests.cs
+++ OpenBookNetUnitTest/UsersUnitTests.cs
@@ -2,7 +2,6 @@
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenBook;
-using OpenBook.Models;
 using OpenBook.Models.ApiResults;
 
 namespace OpenBookNetUnitTest
@@ -48,6 +47,29 @@ namespace OpenBookNetUnitTest
                 Assert.IsNotNull(userData.LastActivity.Activity);
                 Assert.IsNotNull(userData.LastActivity.Market);
                 Assert.IsNotNull(userData.LastActivity.Owner);
+            }
+        }
+
+        [TestMethod]
+        public void GetTopPerformers()
+        {
+            var result = _client.GetTopPerformers().Result;
+
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(result.TopPerformers);
+            Assert.IsTrue(result.TopPerformers.Count > 0);
+
+            foreach (var topPerformerData in result.TopPerformers)
+            {
+                Assert.IsNotNull(topPerformerData);
+                Assert.IsNotNull(topPerformerData.Users);
+
+                foreach (var topUserData in topPerformerData.Users)
+                {
+                    Assert.IsNotNull(topUserData);
+                    Assert.IsNotNull(topUserData.User);
+                    Assert.IsNotNull(topUserData.Ranking);
+                }
             }
         }
 
